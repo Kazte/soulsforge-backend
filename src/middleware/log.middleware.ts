35e005f4230151dbfad9be36fa96b8d938b65198
Kazte <39470734+Kazte@users.noreply.github.com>
2023-08-09 23:19:09 -0300

    [feat] - log middleware

--- src/middleware/log.middleware.ts
+++ src/middleware/log.middleware.ts
@@ -2,9 +2,9 @@ import { NextFunction, Request, Response } from 'express';
 import { logInfo } from '../utilities/log.utility';
 
 const logMiddleware = (req: Request, res: Response, next: NextFunction) => {
-	const stringBuilder = `${req.method} ${req.originalUrl} ${req.ip} ${req.get('user-agent')}`;
-
-	logInfo(`${stringBuilder}`);
+	res.on('finish', function () {
+		logInfo(`[${new Date().toLocaleString()}] ${req.originalUrl} ${req.method} (${res.statusCode})`);
+	});
 	next();
 };
 
